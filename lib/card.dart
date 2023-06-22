dfe7beed360d3d728700005de068a1d9eb223eb3
kmoorthy-presidio <126165321+Keer-thi-vasan-m@users.noreply.github.com>
2023-06-22 11:40:34 +0530

    Home Screen - list view

--- lib/card.dart
+++ lib/card.dart
@@ -1,29 +1,199 @@
+import 'package:bolt/main.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:pattern_formatter/pattern_formatter.dart';
+import 'package:flutter/services.dart';
 
-void card() {
-  runApp(const CardExamplesApp());
+class CtDropdown extends StatefulWidget {
+  // const Dropdown({Key? key}) : super(key: key);
+  String dropdownvalue;
+  final List<String> items;
+
+  CtDropdown(this.items, this.dropdownvalue);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class CardExamplesApp extends StatelessWidget {
-  const CardExamplesApp({super.key});
+class _MyHomePageState extends State<CtDropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(left: 10, top: 1, right: 10),
+        height: 45,
+        width: 100,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 5.0, // soften the shadow
+              spreadRadius: 1.0, //extend the shadow
+              offset: Offset(
+                2.0, // Move to right 10  horizontally
+                2.0, // Move to bottom 10 Vertically
+              ),
+            )
+          ],
+          //<-- SEE HERE
+        ),
+        child: new DropdownButtonHideUnderline(
+          child: DropdownButton(
+            isExpanded: true,
+            // Initial Value
+            value: widget.dropdownvalue,
+            dropdownColor: Colors.white,
+            // Down Arrow Icon
+            icon: const Icon(Icons.keyboard_arrow_down, color: Colors.black),
+            // Array list of items
+            items: widget.items.map((String items) {
+              return DropdownMenuItem(
+                value: items,
+                child: Center(
+                  child: Text(
+                    items,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              );
+            }).toList(),
+            // After selecting the desired option,it will
+            // change button value to selected value
+            onChanged: (String? newValue) {
+              setState(() {
+                widget.dropdownvalue = newValue!;
+              });
+            },
+          ),
+        ));
+  }
+}
+
+class AmountEntryWidget extends StatefulWidget {
+  @override
+  _AmountEntryWidgetState createState() => _AmountEntryWidgetState();
+}
+
+class _AmountEntryWidgetState extends State<AmountEntryWidget> {
+  late TextEditingController _amountController;
+
+  @override
+  void initState() {
+    super.initState();
+    _amountController = TextEditingController();
+  }
+
+  final _controller = TextEditingController();
+  static const _locale = 'en';
+
+  String _formatNumber(String s) =>
+      NumberFormat.decimalPattern(_locale).format(int.parse(s));
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Card Examples')),
-        body: Row(
-          children: const <Widget>[
-            Spacer(),
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 150.0),
+          Container(
+            width: 500,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/rs.png',
+                  scale: 1,
+                ),
+                Container(
+                  width: 150,
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: "Karla",
+                        fontWeight: FontWeight.w700,
+                        height: 1.5,
+                        letterSpacing: 0.15,
+                        color: Colors.black),
+                    inputFormatters: [
+                      ThousandsFormatter(),
+                      LengthLimitingTextInputFormatter(6),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 40.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CtDropdown(["card", "cash"], "cash"),
+              Image.asset(
+                'images/arrow-right.png',
+                scale: 1,
+              ),
+              CtDropdown(["food", "others"], "food"),
+            ],
+          ),
+          SizedBox(height: 30.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                '12 april, 10:00 am',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0.15,
+                  height: 1.5,
+                  fontFamily: 'Karla',
+                  color: Color(0xff686868),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  onPrimary: Colors.black,
+                  primary: Color(0xffFBFF4A),
+                  // Set the background color
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Raleway',
+                    fontSize: 18.0, // Set the font size
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
 
-            Spacer(),
-          ],
-        ),
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 10.0), // Set the padding
+                ),
+                onPressed: () {
+                  // Handle button press
+                },
+                child: Text(
+                  'save',
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
 }
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'card.dart';
+import 'package:intl/intl.dart';
+
 final List<String> imgList = [
   'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
   'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
@@ -9,24 +13,268 @@ final List<String> imgList = [
   'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
   'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
 ];
-void main()
-{
 
-    runApp(
-         MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            appBarTheme: AppBarTheme(
-              iconTheme: IconThemeData(color: Colors.red),
-              foregroundColor: Colors.red, //<-- SEE HERE
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.red),
+          foregroundColor: Colors.red, //<-- SEE HERE
+        ),
+      ),
+      home: Navbar(),
+    ),
+  );
+}
+
+class TextImageCard extends StatelessWidget {
+  final String text;
+  final String imageUrl;
+
+  TextImageCard({required this.text, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(imageUrl), // Display the image
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(text), // Display the text
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ContentDisplay extends StatelessWidget {
+  const ContentDisplay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [Container()],
+    ));
+  }
+}
+
+class NoonLoopingDemo extends StatelessWidget {
+  const NoonLoopingDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const itemLength = 4;
+    return Container(
+      margin: EdgeInsets.only(top: 39),
+      width: 340,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+        ),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20, bottom: 0, top: 19),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Expenses',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Raleway",
+                          color: Colors.black,
+                          letterSpacing: 0.15,
+                          height: 1.5,
+                          fontWeight: FontWeight.w700,
+                        ), //<-- SEE HERE
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      Dropdown(["week", "month"], "week"),
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      Dropdown(["food", "other", "credit"], "other"),
+                      // Dropdown(["ll","k"]),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 50, bottom: 10, top: 35),
+                  child: Text(
+                    'may 2023',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: "Karla",
+                      color: Colors.black,
+                      letterSpacing: 0.15,
+                      fontWeight: FontWeight.w500,
+                    ), //<-- SEE HERE
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(right: 50, bottom: 10, top: 35),
+                  child: Image.asset(
+                    'images/add.png',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20, top: 6, bottom: 14),
+                  child: Container(
+                    child: Image.asset(
+                      'images/Rectangle 5.png',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 220,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: itemLength,
+              itemBuilder: (BuildContext context, int index) {
+                // if (index == itemLength - 1) // here will be the logic
+                {
+                  return Column(
+                    children: [
+                      SizedBox(
+                        height: 10,
+                        width: 30,
+                      ),
+                      Row(
+                        children: [
+                          // Icxon(Icons.arrow_back),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Container(
+                            width: 80,
+                            height: 110,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'images/thunder.png',
+                                  scale: 6,
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontFamily: "Raleway",
+                                      color: Colors.white,
+                                      letterSpacing: 0.15,
+                                      height: 1.5,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                    "data")
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  );
+                }
+              },
             ),
           ),
-            home: Navbar(),
-        ),
-
+        ],
+      ),
     );
+  }
+}
 
+class Dropdown extends StatefulWidget {
+  // const Dropdown({Key? key}) : super(key: key);
+  String dropdownvalue;
+  final List<String> items;
+
+  Dropdown(this.items, this.dropdownvalue);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Dropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(left: 10, top: 1, right: 10),
+        height: 28,
+        width: 95,
+        decoration: BoxDecoration(
+          color: Color(0xff2D2D2D),
+          borderRadius: BorderRadius.circular(20), //<-- SEE HERE
+        ),
+        child: new DropdownButtonHideUnderline(
+          child: DropdownButton(
+            // Initial Value
+            value: widget.dropdownvalue,
+            dropdownColor: Color(0xff2D2D2D),
+            // Down Arrow Icon
+            icon: const Icon(Icons.keyboard_arrow_down, color: Colors.white),
+            // Array list of items
+            items: widget.items.map((String items) {
+              return DropdownMenuItem(
+                value: items,
+                child: Text(
+                  items,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 15.0,
+                  ),
+                ),
+              );
+            }).toList(),
+            // After selecting the desired option,it will
+            // change button value to selected value
+            onChanged: (String? newValue) {
+              setState(() {
+                widget.dropdownvalue = newValue!;
+              });
+            },
+          ),
+        ));
+  }
 }
 
 class Navbar extends StatelessWidget {
@@ -34,124 +282,126 @@ class Navbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-          toolbarHeight: 100.0, // double
+    return Scaffold(
+        appBar: AppBar(
+          bottomOpacity: 0.0,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          toolbarHeight: 100.0,
+          // double
           automaticallyImplyLeading: false,
           flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.deepPurpleAccent, Colors.yellow], stops: [0.5, 1],
+            decoration: BoxDecoration(color: Colors.white),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/main_icon.png',
+                scale: 1,
               ),
-            ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text(
+                'Bolt',
+                style: TextStyle(
+                    fontSize: 35,
+                    fontFamily: "Raleway",
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 0.15,
+                    color: Colors.black),
+              ),
+            ],
           ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'images/thunder.png',
-              scale: 5,
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            const Text(
-              'Bolt',
-              style: TextStyle(fontSize: 35,fontFamily: "Raleway",color: Colors.white), //<-- SEE HERE
-            ),
-          ],
         ),
-          elevation: 0.0,
-      ),
-      body:NoonLoopingDemo()
 
-
-
-    )
-    ;
+        // need Route it
+        body: AmountEntryWidget());
+    // body: Container(
+    //   decoration: BoxDecoration(color: Colors.white),
+    //   child: Column(
+    //     children: [
+    //       Stack(clipBehavior: Clip.none, children: [
+    //         Container(
+    //           constraints: new BoxConstraints(
+    //             minHeight: 113,
+    //             maxHeight: 130,
+    //           ),
+    //           margin: EdgeInsets.only(top: 50.0, left: 47, right: 47),
+    //           padding: EdgeInsets.all(20),
+    //           decoration: new BoxDecoration(
+    //             color: Colors.black,
+    //             borderRadius: BorderRadius.circular(19),
+    //           ),
+    //           child: Row(
+    //             children: [
+    //               Expanded(
+    //                   child: Column(children: [
+    //                 Text(
+    //                   'Current Spends',
+    //                   style: TextStyle(
+    //                     color: Colors.white,
+    //                     fontFamily: 'Raleway',
+    //                     fontWeight: FontWeight.w700,
+    //                     fontSize: 15.0,
+    //                     height: 2.5,
+    //                   ),
+    //                 ),
+    //                 Text(
+    //                   '₹' + '1000.00',
+    //                   style: TextStyle(
+    //                     color: Color(0xffFBFF4A),
+    //                     fontFamily: 'Karla',
+    //                     fontWeight: FontWeight.w700,
+    //                     fontSize: 25.0,
+    //                     height: 1.5,
+    //                   ),
+    //                 )
+    //               ])),
+    //               Expanded(
+    //                   child:
+    //                       // Second column content
+    //                       Column(children: [
+    //                 // Dropdown(),
+    //               ])),
+    //             ],
+    //           ),
+    //         ),
+    //         Positioned(
+    //             bottom: 92,
+    //             right: 60,
+    //             child: Padding(
+    //               padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    //               child: Row(
+    //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //                 children: [
+    //                   Dropdown(["week", "month"], "week"),
+    //                 ],
+    //               ),
+    //             )),
+    //         Positioned(
+    //             bottom: -18,
+    //             right: 55,
+    //             child: Padding(
+    //               padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    //               child: Row(
+    //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //                 children: [
+    //                   const SizedBox(
+    //                     width: 0,
+    //                   ),
+    //                   Image.asset(
+    //                     'images/man_with_board.png',
+    //                     scale: 0.9,
+    //                   ),
+    //                 ],
+    //               ),
+    //             ))
+    //       ]),
+    //       NoonLoopingDemo(),
+    //     ],
+    //   ),
+    // ));
   }
 }
-
-class NoonLoopingDemo extends StatelessWidget {
-  const NoonLoopingDemo({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    for (var pet in imgList) {
-      print(pet);
-    }
-    return
-      Container(
-        child:
-        Column(
-          children: [
-             Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Padding(
-                        padding: EdgeInsets.only(left:50,bottom:10,top:50),
-
-                        child: Text(
-                          'Bolt',
-                          style: TextStyle(fontSize: 25,fontFamily: "Raleway",color: Colors.red), //<-- SEE HERE
-                        ),
-                      ),
-                    ),
-                    Container(
-                      child: Padding(
-                        padding: EdgeInsets.only(right:50,bottom:10,top:50),
-                        child:
-                        Image.asset(
-                          'images/thunder.png',
-                          scale: 5,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-
-
-        Container(
-          height:220,
-          child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (BuildContext context, int index) {
-          return Column(
-          children: [
-
-          SizedBox(
-          height: 10,
-          width: 30,
-          ),
-          Row(
-          children: [
-          SizedBox(
-          width: 30,
-          ),
-          Image.asset(
-          'images/thunder.png',
-          scale: 2,
-          ),
-
-          ],
-          ),
-
-          ],
-          );
-
-          },
-          ),
-        ),
-              ],
-        ),
-
-
-
-      );
-  }
-}
-
-
